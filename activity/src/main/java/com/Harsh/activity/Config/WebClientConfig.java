f7745b032a8b4ad46f4cc4c02f2070eb10e4849b
Harsh Gupta <harshgupta88156@gmail.com>
2025-09-27 01:49:18 +0530

    ADDED GEMINI API MODEL

--- activity/src/main/java/com/Harsh/activity/Config/WebClientConfig.java
+++ activity/src/main/java/com/Harsh/activity/Config/WebClientConfig.java
@@ -17,6 +17,6 @@ public class WebClientConfig {
 
     @Bean
     public WebClient userServiceWebClient(WebClient.Builder webClientBuilder){
-        return webClientBuilder.baseUrl("http://USERSERVICE").build();
+        return webClientBuilder.baseUrl("http://USER-SERVICE").build();
     }
 }
